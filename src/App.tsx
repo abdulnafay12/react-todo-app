161f00500f79f672aa1a349273add49591cacbf5
Muhammad Nafay <160539906+abdulnafay12@users.noreply.github.com>
2024-05-23 17:48:58 +0530

    first comit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import QuizScreen from './pages/QuizScreen';
+import APIHandling from './pages/API HandlingScreen';
+
+function App() {
+  return <APIHandling />;
+}
+
+export default App;
